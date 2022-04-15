dd33b0b5372e646a38355aff5341ee0ee440e235
Am05gautam <am05gautam@gmail.com>
2022-04-16 00:27:47 +0530

    changes done in app.js

--- app.js
+++ app.js
@@ -1,57 +1,68 @@
-const express = require('express');
+const express = require("express");
 const bodyParser = require("body-parser");
-const morgan = require('morgan');
-const cors = require('cors');
+const morgan = require("morgan");
+const cors = require("cors");
 
 const app = express();
-const multer = require('multer');
+const multer = require("multer");
 
-const mongoose = require('mongoose');
-const dotenv = require('dotenv');
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
 dotenv.config();
 // console.log(process.env.MONGODB_USER);
 
 const fileStorage = multer.diskStorage({
-    destination: (req, file, cb) => {
-        cb(null, 'files');
-    },
-    filename: (req, file, cb) => {
-        cb(null, file.originalname);
-    }
+  destination: (req, file, cb) => {
+    cb(null, "files");
+  },
+  filename: (req, file, cb) => {
+    cb(null, file.originalname);
+  },
 });
 
 const fileFilter = (req, file, cb) => {
-    if (file.mimetype === 'file/pdf' || file.mimetype === 'file/png' || file.mimetype === 'file/jpg' || file.mimetype === 'file/jpeg'){
-        cb(null, true);
-    }
-    else{
-        cb(null, false);
-    }
-}
+  if (
+    file.mimetype === "file/pdf" ||
+    file.mimetype === "file/png" ||
+    file.mimetype === "file/jpg" ||
+    file.mimetype === "file/jpeg"
+  ) {
+    cb(null, true);
+  } else {
+    cb(null, false);
+  }
+};
 
 app.use(morgan("dev"));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(cors());
-app.use(multer({
+app.use(
+  multer({
     storage: fileStorage,
-    fileFilter: fileFilter
-}).array('file')
+    fileFilter: fileFilter,
+  }).array("file")
 );
 
-const dbUrl = `mongodb+srv://${process.env.MONGODB_USER}:${process.env.MONGODB_PASSWORD}@cluster0.ohrbj.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`
+const dbUrl = `mongodb+srv://${process.env.MONGODB_USER}:${process.env.MONGODB_PASSWORD}@cluster0.ohrbj.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 
-mongoose.connect(dbUrl, {
+mongoose
+  .connect(dbUrl, {
     useNewUrlParser: true,
-    useUnifiedTopology: true
-}).then(result => console.log('MongoDB Connected!'))
-    .catch(err => console.log(err));
+    useUnifiedTopology: true,
+  })
+  .then((result) => console.log("MongoDB Connected!"))
+  .catch((err) => console.log(err));
 
-const userRouter = require('./routes/user.js');
-const authRoutes = require('./routes/auth.js');
+const userRouter = require("./routes/user.js");
+const authRoutes = require("./routes/auth.js");
+
+app.get("/", (req, res) => {
+  res.status(200).json({ message: "This is working" });
+});
 
 app.use("/user", userRouter);
-app.use('/auth', authRoutes);
+app.use("/auth", authRoutes);
 
 // //Handle production
 // if (process.env.NODE_ENV === 'production') {
@@ -65,5 +76,5 @@ app.use('/auth', authRoutes);
 const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
-    console.log(`Server Running!`);
-})
+  console.log(`Server Running!`);
+});
